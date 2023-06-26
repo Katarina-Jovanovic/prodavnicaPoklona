e23a70fde57bb0714d45b60023a5c94ed53df82d
Katarina-Jovanovic <katarina.jovanovic.4556@metropolitan.ac.rs>
2023-06-26 15:26:14 +0200

    Initial commit

---
+++ src/main/java/com/example/prodavnicaPoklona/ProdavnicaPoklonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prodavnicaPoklona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdavnicaPoklonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdavnicaPoklonaApplication.class, args);
+	}
+
+}
